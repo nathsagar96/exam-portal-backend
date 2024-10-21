052acd3c570f4152c8e7ea670ab968be8584a9a1
nathsagar96 <nathsagar96@gmail.com>
2024-10-21 21:53:46 +0530

    first commit

---
+++ src/test/java/org/examportal/TestApplication.java
@@ -0,0 +1,11 @@
+package org.examportal;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestContainersConfiguration.class).run(args);
+	}
+
+}
